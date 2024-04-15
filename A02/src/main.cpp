a9aea90f0d3a7952b00c88d93e44663fe62a71e9
Felipe Beserra <felipebeserra25@gmail.com>
2024-04-15 11:49:03 -0300

    Code tested in class

--- A02/src/main.cpp
+++ A02/src/main.cpp
@@ -1,49 +1,48 @@
 #include "mbed.h"
 #include "nRF24L01P.h"
-
-// #include <algorithm>
-// #include <list>
-
-// #include <Package.h>
+#include <algorithm>
 
 using namespace std;
+
+#define TRANSFER_SIZE   3  // 3 bytes na forma de 3 caracteres (3 bytes = 28 bits)
+#define ACK_TIMEOUT 500000 //us
 
 // Hardware
 Serial pc(USBTX, USBRX); // tx, rx
 nRF24L01P my_nrf24l01p(PA_7, PA_6, PA_5, PA_9, PC_7, PA_8);    // mosi, miso, sck, csn, ce, irq
 
+// Incoming Package information
+char rxData[TRANSFER_SIZE];
+int rxDataCnt = 0;   //Bytes recebidos
 
-// TODO: Protocolo stop-and-wait
+// Outgoing Package information
+int countAckg = 0;
+char txData[TRANSFER_SIZE]={3, countAckg, 1};
 
-// mandar pkg --> esperar pra ver se tem resposta
-// se receber akg --> esperar ele enviar um pacote
-// se não receber ack --> reenviar o pacote --> até receber akg
+// LOGIC
+Timeout ackger;
+bool timedOut = false; // Control TRUE when timeout
+bool ackged = false;
+int contador = 1;
 
-#define TRANSFER_SIZE 3 // 28 bits
+void sendData(){
+    // Update Count
+    txData[1] = countAckg % 2;
 
+    my_nrf24l01p.write(NRF24L01P_PIPE_P0, txData, TRANSFER_SIZE);
+    pc.printf("Enviei %d, %d, %d pela %d vez \r\n", txData[0],txData[1],txData[2],contador);
+    contador++;
+}
 
-void retransmit_ack() {
-    
+void callbackTimeout() {
+    pc.printf("Timed out!\r\n");
+    timedOut = true;
+
+    if (!ackged)
+        sendData();
 }
 
 int main() {
-
-    // Create new package to send
-    OutgoingPackages.push_back(new Package({}, 0, {3, 21, 32}, ));
-
-
-    // See if there is any new data incoming
-
-
-
-    // Num de bytes tx = num de bytes rx
-    char rxData[TRANSFER_SIZE];
-    int rxDataCnt = 0;   //Bytes recebidos
-
-    char txData[TRANSFER_SIZE]=;
-    int txDataCnt = TRANSFER_SIZE;
-    unsigned int contador=0;
-    
     my_nrf24l01p.powerUp();   //Aciona a NRF24L01+
 
     my_nrf24l01p.setTransferSize( TRANSFER_SIZE );  //Determina tamanho do payload
@@ -64,40 +63,41 @@ int main() {
     my_nrf24l01p.enable();
     
     
-    while(1){
+    while(true){
+
         // Read incoming data for starting
-        if (pc.getc() == 't') {
-
-            // Envia o conteúdo de txData via nRF24L01+
-            my_nrf24l01p.write( NRF24L01P_PIPE_P0, txData, txDataCnt);
-            contador++;
-                
-            pc.printf("Enviei %d, %d, %d pela %d vez \r\n", txData[0],txData[1],txData[2],contador);
-                
-            wait(0.8);  //Espera 0.8s para transmitir   
+        if (pc.getc() == 't') { // pc.getc --> STOPS EXECUTION OF THREAD!
+            sendData();
+            ackged = false;
+            contador = 1;
+            wait(0.8);  // Espera 0.8s para transmitir   
         }
 
-        bool valid_response = false;
-        bool ack_recived = false;
+        while(!ackged) {
+            timedOut = false;
+            ackger.attach_us(&callbackTimeout, ACK_TIMEOUT);
 
-        ackger.attach_us(&retransmit_ack, ACK_TIMEOUT);
+            while (!ackged && !timedOut ) {
+                while (!my_nrf24l01p.readable() ) { } // .readable() --> STOPS EXECUTION OF THREAD!
 
-        while (!valid_response) {
+                // Leia o que está na fila de recepção
+                rxDataCnt = my_nrf24l01p.read( NRF24L01P_PIPE_P0, rxData, sizeof( rxData ) );
 
-            // Pooling: Verifica se a NRF24L01+ recebeu algo
-            while ( !my_nrf24l01p.readable() ) { }
+                // Pacote para nosso endereço recebido
+                if (rxData[0] == 3) {
+                    pc.printf("Pacote recebido!!\r\n");
+                    pc.printf("TRANSMITIDO: %d, %d, %d\r\n", txData[0], txData[1], txData[2]);
+                    pc.printf("RECEBIDO: %d, %d, %d\r\n", rxData[0], rxData[1], rxData[2]);
 
-            // Leia o que está na fila de recepção
-            rxDataCnt = my_nrf24l01p.read( NRF24L01P_PIPE_P0, rxData, sizeof( rxData ) );
-
-            if (rxData[0] == 3) {
-                valid_response = true;
-             
-                // Mostre os 3 bytes recebidos através da terminal conectado na porta serial
-                pc.printf("Valores recebidos: %d, %d, %d\r\n", rxData[0],rxData[1],rxData[2]);
+                    // ACK recebido
+                    ackged = (txData[0] == rxData[0] && txData[1] == rxData[1] && txData[2] == rxData[2]);                    
+                }
             }
+
+            ackger.detach();
         }
-        
-        ackger.detach();   
+
+        pc.printf("ACK recebido!\r\n");
+        countAckg++;
     }
-}
+};
